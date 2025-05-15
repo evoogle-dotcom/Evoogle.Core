6aa31b445f02a89009ef913d392e074289cbae60
Scott McDonald <scott@evoogle.com>
2025-05-14 20:18:43 -0400

    Add NullJsonNamingPolicy
    Signed-off-by: Scott McDonald <scott@evoogle.com>

---
+++ Source/Evoogle.Core/Json/NullJsonNamingPolicy.cs
@@ -0,0 +1,27 @@
+using System.Text.Json;
+
+namespace Evoogle.Json;
+
+/// <summary>
+///     A null object implementation of <see cref="JsonNamingPolicy"/> that performs no conversion on property names.
+///     This class can be used when no naming policy is desired, effectively leaving property names unchanged during JSON serialization and deserialization.
+/// </summary>
+/// <remarks>
+///     This policy is useful in scenarios where the default naming behavior is desired, or when integrating with systems that expect property names to match exactly.
+///     It can also serve as a fallback or default policy when no specific naming transformation is required.
+/// </remarks>
+public class NullJsonNamingPolicy : JsonNamingPolicy
+{
+    #region JsonNamingPolicy Methods
+    /// <summary>
+    ///     Overrides the <see cref="JsonNamingPolicy.ConvertName"/> method to return the input name without any conversion.
+    ///     This ensures that property names remain unchanged during JSON operations.
+    /// </summary>
+    /// <param name="name">The name to convert.</param>
+    /// <returns>The original name, unchanged.</returns>    
+    public override string ConvertName(string name)
+    {
+        return name;
+    }
+    #endregion
+}
