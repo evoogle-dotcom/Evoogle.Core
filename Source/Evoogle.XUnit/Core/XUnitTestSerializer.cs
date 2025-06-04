45e52497f95c3510bd3ea68b0e958719e9f669ac
Scott McDonald <scott@evoogle.com>
2025-06-03 23:52:45 -0400

    Add missing using statements for XUnitTestSerializer
    Signed-off-by: Scott McDonald <scott@evoogle.com>

--- Source/Evoogle.XUnit/Core/XUnitTestSerializer.cs
+++ Source/Evoogle.XUnit/Core/XUnitTestSerializer.cs
@@ -3,6 +3,14 @@
 //
 // This file is licensed under the MIT License.
 // See the LICENSE file in the project root for more information.
+using System.Diagnostics.CodeAnalysis;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+using Evoogle.Json;
+
+using Xunit.Sdk;
+
 namespace Evoogle.XUnit;
 
 /// <summary>
