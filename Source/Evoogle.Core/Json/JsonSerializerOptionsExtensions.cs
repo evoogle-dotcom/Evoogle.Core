0bab50d127a23f73cdf5ed5e10e3c2c647daf5d8
Scott McDonald <scott@evoogle.com>
2025-10-18 02:27:53 -0400

    Add JsonSerializerOptionsExtenions to add extension methods to JsonSerializerOptions
    Signed-off-by: Scott McDonald <scott@evoogle.com>

---
+++ Source/Evoogle.Core/Json/JsonSerializerOptionsExtensions.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) 2024-2025 Evoogle.com
+// SPDX-License-Identifier: MIT
+//
+// This file is licensed under the MIT License.
+// See the LICENSE file in the project root for more information.
+using System.Text.Json;
+
+namespace Evoogle.Json;
+
+/// <summary>
+///     Extension methods for .NET <see cref="JsonSerializerOptions"> class.
+/// </summary>
+public static class JsonSerializerOptionsExtensions
+{
+    #region Methods
+    /// <summary>
+    ///    Gets the property naming policy, or a null-object if none is set.
+    /// </summary>
+    /// <param name="options">The JSON serializer options.</param>
+    /// <returns>Non-null property naming policy.</returns>
+    public static JsonNamingPolicy GetPropertyNamingPolicy(this JsonSerializerOptions options) => options.PropertyNamingPolicy ?? new NullJsonNamingPolicy();
+    #endregion
+}
