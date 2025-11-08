d4900b9a0fb6e0eae3c71ddffc203e9af52309ef
Scott McDonald <scott@evoogle.com>
2025-11-08 00:04:28 -0500

    Add another overload of WriteJsonArray that takes a ReadOnlySpan to enumerate over
    Signed-off-by: Scott McDonald <scott@evoogle.com>

--- Source/Evoogle.Core/Json/JsonConverterBase.Write.cs
+++ Source/Evoogle.Core/Json/JsonConverterBase.Write.cs
@@ -4,6 +4,7 @@
 // This file is licensed under the MIT License.
 // See the LICENSE file in the project root for more information.
 using System.Text.Json;
+using System;
 
 namespace Evoogle.Json;
 
@@ -34,6 +35,35 @@ public abstract partial class JsonConverterBase<T>
             foreach (var item in array)
             {
                 writeItem(item);
+            }
+        }
+        finally
+        {
+            writer.WriteEndArray();
+        }
+    }
+
+    /// <summary>
+    ///    Writes a JSON array using the provided writer and item writing action.
+    ///    Overload optimized for contiguous memory via ReadOnlySpan to avoid allocations.
+    /// </summary>
+    /// <typeparam name="TItem">The type of items in the array.</typeparam>
+    /// <param name="writer">The JSON writer.</param>
+    /// <param name="array">The contiguous span of items to write.</param>
+    /// <param name="writeItem">An action that writes a single item to the JSON writer.</param>
+    protected static void WriteJsonArray<TItem>
+    (
+        Utf8JsonWriter writer,
+        ReadOnlySpan<TItem> array,
+        Action<TItem> writeItem
+    )
+    {
+        writer.WriteStartArray();
+        try
+        {
+            for (var i = 0; i < array.Length; i++)
+            {
+                writeItem(array[i]);
             }
         }
         finally
