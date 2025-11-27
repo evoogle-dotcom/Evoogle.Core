3cc5f720077c5fdefc9e0798d7fd1558eee321a6
Scott McDonald <scott@evoogle.com>
2025-11-27 02:57:32 -0500

    Remove unnecessary usings
    Signed-off-by: Scott McDonald <scott@evoogle.com>

--- Source/Evoogle.Core/Json/JsonConverterBase.Write.cs
+++ Source/Evoogle.Core/Json/JsonConverterBase.Write.cs
@@ -3,7 +3,6 @@
 //
 // This file is licensed under the MIT License.
 // See the LICENSE file in the project root for more information.
-using System;
 using System.Text.Json;
 
 namespace Evoogle.Json;
