ae497725caa3b14e52131a8ee08750be269a7986
Scott McDonald <scott@evoogle.com>
2026-06-02 03:00:21 -0400

    Update nuget references
    Signed-off-by: Scott McDonald <scott@evoogle.com>

--- Source/Evoogle.Core/Logging/MultiplexingLoggerMode.cs
+++ Source/Evoogle.Core/Logging/MultiplexingLoggerMode.cs
@@ -3,6 +3,10 @@
 //
 // This file is licensed under the MIT License.
 // See the LICENSE file in the project root for more information.
+using System.Diagnostics;
+
+using Microsoft.Extensions.Logging;
+
 namespace Evoogle.Logging;
 
 /// <summary>
@@ -18,17 +22,17 @@ public enum MultiplexingLoggerMode
     None = 0,
 
     /// <summary>
-    ///     Log to the provided <see cref="Microsoft.Extensions.Logging.ILogger"/>, if available.
+    ///     Log to the provided <see cref="ILogger"/> instance, if available.
     /// </summary>
     Logger = 1 << 0,
 
     /// <summary>
-    ///     Log to <see cref="System.Diagnostics.Debug.WriteLine(string)"/> if a debugger is attached.
+    ///     Log to <see cref="Debug.WriteLine(string)"/> if a debugger is attached.
     /// </summary>
     Debug = 1 << 1,
 
     /// <summary>
-    ///     Log to <see cref="System.Console.WriteLine(string)"/>, regardless of debugger presence.
+    ///     Log to <see cref="Console.WriteLine(string)"/>, regardless of debugger presence.
     /// </summary>
     Console = 1 << 2,
 
